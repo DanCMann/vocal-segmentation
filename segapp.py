e06305bc4b835832d63c06e2d5e4a1d42d26c3e7
Dan C. Mann <danmann23@gmail.com>
2022-07-27 14:05:37 +0200

    adds f0 contour plotting.

--- segapp.py
+++ segapp.py
@@ -19,9 +19,120 @@ class Syllable(parselmouth.Sound):
         self.filename = filename
         self.sound = parselmouth.Sound(self.filename)
         self.spectrogram = self.sound.to_spectrogram()
+        
         self.intensity = self.sound.to_intensity(minimum_pitch = 400)
-        self.f0 = self.sound.to_pitch_ac(pitch_ceiling = 3000, pitch_floor = 500)
+        self._time_step = None
+        self._pitch_floor = 500 
+        self._max_num_of_candidates = 15
+        self._very_accurate = False
+        self._silence_threshold = 0.03
+        self._voicing_threshold = 0.45
+        self._octave_cost = 0.01
+        self._octave_jump_cost = 0.35
+        self._voiced_unvoiced_cost = 0.14
+        self._pitch_ceiling = 15000
+        
+        self.get_f0()
 
+    def get_f0(self):
+        self.f0 = self.sound.to_pitch_ac(time_step = self._time_step, 
+                pitch_floor = self._pitch_floor,
+                max_number_of_candidates = self._max_num_of_candidates,
+                #very_accurate = self._very_accurate,
+                silence_threshold = self._silence_threshold,
+                voicing_threshold = self._voicing_threshold,
+                octave_cost = self._octave_cost,
+                octave_jump_cost = self._octave_jump_cost,
+                voiced_unvoiced_cost = self._voiced_unvoiced_cost,
+                pitch_ceiling = self._pitch_ceiling
+                )
+        #print(self.f0)
+        print(self._time_step)
+        print(self._pitch_floor)
+        print(self._pitch_ceiling)
+
+    @property
+    def time_step(self):
+        return self._time_step
+    # using time_step.setter caused problems with the pyqt5 connect function
+    #  you can't assign in connect
+    # this seems to work for now but I'm sure it'll fuck me over down the line
+    def time_step(self, new_time_step):
+        self._time_step = new_time_step
+        self.get_f0()
+    
+    @property
+    def pitch_floor(self):
+        return self._pitch_floor
+
+    def pitch_floor(self, new_pitch_floor):
+        self._pitch_floor = new_pitch_floor
+        self.get_f0()
+    
+    @property
+    def max_num_of_candidates(self):
+        return self._max_num_of_candidates
+    
+    def max_num_of_candidates(self, new_max_num_cand):
+        self._max_num_of_candidates = new_max_num_cand
+        self.get_f0()
+    
+    @property
+    def very_accurate(self):
+        return self._very_accurate
+
+    def very_accurate(self, new_very_accurate):
+        self._very_accurate = new_very_accurate
+        self.get_f0()
+
+    @property
+    def silence_threshold(self):
+        return self._silence_threshold
+    
+    def silence_threshold(self, new_silence_threshold):
+        self._silence_threshold = new_silence_threshold
+        self.get_f0()
+
+    @property
+    def voicing_threshold(self):
+        return self._voicing_threshold
+
+    def voicing_threshold(self, new_voicing_threshold):
+        self._voicing_threshold = new_voicing_threshold
+        self.get_f0()
+    
+    @property
+    def octave_cost(self):
+        return self._octave_cost
+    
+    def octave_cost(self, new_octave_cost):
+        self._octave_cost = new_octave_cost
+        self.get_f0()
+
+    @property
+    def octave_jump_cost(self):
+        return self._octave_jump_cost
+    
+    def octave_jump_cost(self, new_octave_jump_cost):
+        self._octave_jump_cost = new_octave_jump_cost
+        self.get_f0()
+    
+    @property
+    def voiced_unvoiced_cost(self):
+        return self._voiced_unvoiced_cost
+
+    def voiced_unvoiced_cost(self, new_voiced_unvoiced_cost):
+        self._voiced_unvoiced_cost = new_voiced_unvoiced_cost
+        self.get_f0()
+    
+    @property
+    def pitch_ceiling(self):
+        return self._pitch_ceiling
+    
+    def pitch_ceiling(self, new_pitch_ceiling):
+        self._pitch_ceiling = new_pitch_ceiling
+        self.get_f0()
+ 
     def update_intensity(self, *args, **kwargs):
         self.intensity = self.sound.to_intensity(*args, **kwargs)
 
@@ -63,6 +174,7 @@ class Syllable(parselmouth.Sound):
     
     def draw_f0(self, canvas_axes):
         pitch_values = self.f0.selected_array['frequency']
+        print(pitch_values)
         pitch_values[pitch_values==0] = np.nan
         canvas_axes.plot(self.f0.xs(), pitch_values, 'o', markersize=5, color='w')
         canvas_axes.plot(self.f0.xs(), pitch_values, 'o', markersize=2)
@@ -147,28 +259,45 @@ class TableWidget(QtWidgets.QWidget):
 
         self.f0_pitch_floor = QtWidgets.QSpinBox()
         self.f0_pitch_floor.setRange(50, 5000)
-        self.f0_pitch_floor.setValue(75)
-        self.f0_pitch_floor.setSingleStep(5)
+        self.f0_pitch_floor.setValue(500)
+        self.f0_pitch_floor.setSingleStep(10)
         
         self.f0_max_n_cand = QtWidgets.QSpinBox()
         self.f0_max_n_cand.setRange(1, 15)
         self.f0_max_n_cand.setSingleStep(1)
         self.f0_max_n_cand.setValue(15)
-        '''
+        
         # very accurate widget
 
         self.f0_sil_threshold = QtWidgets.QDoubleSpinBox()
-         
-        self.f0_voicing_threshold = QtWidgets.QDoubleSpinBox()
+        self.f0_sil_threshold.setRange(0, 10)
+        self.f0_sil_threshold.setSingleStep(0.05)
+        self.f0_sil_threshold.setValue(0.03)
 
+        self.f0_voicing_threshold = QtWidgets.QDoubleSpinBox()
+        self.f0_voicing_threshold.setRange(0, 10)
+        self.f0_voicing_threshold.setSingleStep(0.1)
+        self.f0_voicing_threshold.setValue(0.45)
+        
         self.f0_octave_cost = QtWidgets.QDoubleSpinBox()
+        self.f0_octave_cost.setRange(0, 10) 
+        self.f0_octave_cost.setSingleStep(0.01) 
+        self.f0_octave_cost.setValue(0.001)
 
         self.f0_octave_jump_cost = QtWidgets.QDoubleSpinBox()
+        self.f0_octave_jump_cost.setRange(0, 10)
+        self.f0_octave_jump_cost.setSingleStep(0.1)
+        self.f0_octave_jump_cost.setValue(0.05)
 
         self.f0_voiced_unvoiced_cost = QtWidgets.QDoubleSpinBox()
+        self.f0_voiced_unvoiced_cost.setRange(0, 10)
+        self.f0_voiced_unvoiced_cost.setSingleStep(0.1)
+        self.f0_octave_jump_cost.setValue(0.14)
 
         self.f0_pitch_ceiling = QtWidgets.QDoubleSpinBox()
-        '''
+        self.f0_pitch_ceiling.setRange(20, 50000)
+        self.f0_pitch_ceiling.setSingleStep(100)
+        self.f0_pitch_ceiling.setValue(15000)
 
         # Segment
         self.update_button = QtWidgets.QPushButton("Update")
@@ -204,10 +333,16 @@ class TableWidget(QtWidgets.QWidget):
 
         # F0 tab widget connections
         self.f0_button.clicked.connect(lambda: self.update_spectrogram(self.canvas_f0, overlay = "f0"))
-        self.f0_time_step.valueChanged.connect(lambda: self.change_f0(time_step = self.f0_time_step.value()))
-        self.f0_pitch_floor.valueChanged.connect(lambda: self.change_f0(pitch_floor = self.f0_pitch_floor.value()))
-        self.f0_max_n_cand.valueChanged.connect(lambda: self.change_f0(max_number_of_candidates = self.f0_max_n_cand.value()))
-
+        self.f0_time_step.valueChanged.connect(lambda: self.syllable.time_step(self.f0_time_step.value()))
+        self.f0_pitch_floor.valueChanged.connect(lambda: self.syllable.pitch_floor(self.f0_pitch_floor.value()))
+        self.f0_max_n_cand.valueChanged.connect(lambda: self.syllable.max_num_of_candidates(self.f0_max_n_cand.value()))
+        self.f0_sil_threshold.valueChanged.connect(lambda: self.syllable.silence_threshold(self.f0_sil_threshold.value())) 
+        self.f0_voicing_threshold.valueChanged.connect(lambda: self.syllable.voicing_threshold(self.f0_voicing_threshold.value()))
+        self.f0_octave_cost.valueChanged.connect(lambda: self.syllable.octave_cost(self.f0_octave_cost.value()))
+        self.f0_octave_jump_cost.valueChanged.connect(lambda: self.syllable.octave_jump_cost(self.f0_octave_jump_cost.value()))
+        self.f0_voiced_unvoiced_cost.valueChanged.connect(lambda: self.syllable.voiced_unvoiced_cost(self.f0_voiced_unvoiced_cost.value()))
+        self.f0_pitch_ceiling.valueChanged.connect(lambda: self.syllable.pitch_ceiling(self.f0_pitch_ceiling.value()))
+        
         # Segmentation tab widget connections
         self.update_button.clicked.connect(self.update_plot)
         self.segment_button.clicked.connect(self.segment_syllable)
@@ -223,11 +358,18 @@ class TableWidget(QtWidgets.QWidget):
         self.amp_tab.grid.addWidget(self.min_pitch_slider, 0, 1, QtCore.Qt.AlignRight | QtCore.Qt.AlignBottom)
 
         # F0 widgets
-        self.f0_tab.grid.addWidget(self.canvas_f0, 0, 0, 2, 2, QtCore.Qt.AlignLeft | QtCore.Qt.AlignBottom)
+        self.f0_tab.grid.addWidget(self.canvas_f0, 0, 0, 3, 3, QtCore.Qt.AlignLeft | QtCore.Qt.AlignBottom)
         self.f0_tab.grid.addWidget(self.f0_button, 4, 4, QtCore.Qt.AlignLeft | QtCore.Qt.AlignBottom)
         self.f0_tab.grid.addWidget(self.f0_time_step, 0, 3, QtCore.Qt.AlignLeft | QtCore.Qt.AlignBottom)
         self.f0_tab.grid.addWidget(self.f0_pitch_floor, 1, 3, QtCore.Qt.AlignLeft | QtCore.Qt.AlignBottom)
         self.f0_tab.grid.addWidget(self.f0_max_n_cand, 2, 3, QtCore.Qt.AlignLeft | QtCore.Qt.AlignBottom)
+
+        self.f0_tab.grid.addWidget(self.f0_sil_threshold, 3, 3, QtCore.Qt.AlignLeft | QtCore.Qt.AlignBottom)
+        self.f0_tab.grid.addWidget(self.f0_voicing_threshold, 4, 3, QtCore.Qt.AlignLeft | QtCore.Qt.AlignBottom)
+        self.f0_tab.grid.addWidget(self.f0_octave_cost, 5, 3, QtCore.Qt.AlignLeft | QtCore.Qt.AlignBottom)
+        self.f0_tab.grid.addWidget(self.f0_octave_jump_cost, 6, 3, QtCore.Qt.AlignLeft | QtCore.Qt.AlignBottom)
+        self.f0_tab.grid.addWidget(self.f0_voiced_unvoiced_cost, 7, 3, QtCore.Qt.AlignLeft | QtCore.Qt.AlignBottom)
+        self.f0_tab.grid.addWidget(self.f0_pitch_ceiling, 8, 3, QtCore.Qt.AlignLeft | QtCore.Qt.AlignBottom)
         
         # Segmentation tab widgets
         self.spect_tab.grid.addWidget(self.canvas, 0, 0, QtCore.Qt.AlignLeft | QtCore.Qt.AlignBottom)
@@ -294,7 +436,8 @@ class TableWidget(QtWidgets.QWidget):
         if(self.syllable is None):
             self.load_sound()
 
-        self.syllable.update_f0(*args, **kwargs)
+        #self.syllable.update_f0(*args, **kwargs)
+        
         self.update_spectrogram(self.canvas_f0, overlay = "f0")
 
     def change_intensity(self, *args, **kwargs):
